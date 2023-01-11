dea9b100d3499d935def5c5ed156c6cf44bf85df
Leology0 <112642025+Leology0@users.noreply.github.com>
2023-01-10 21:31:42 -0700

    za

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 // Shopping Cart Project by Leo
-// Clear cart when page reloads
+// Clear cart when page loads
 localStorage.clear();
 
 // Variables
@@ -7,29 +7,44 @@ localStorage.clear();
 sauceBtnEl = document.getElementById("sauce-btn");
 appleBtnEl = document.getElementById("apple-btn");
 pringlesBtnEl = document.getElementById("pringles-btn");
-outputEl = document.getElementById("output")
+outputEl = document.getElementById("output");
 // Other Variables
-totalCost = 0
+totalCost = 0;
 
 
 // Create shopping cart
-cartArray = []
+items = []
 
+// Display items
+function displayItems() {
+    outputStr = ''
+    for (i = 0; i <= items.length; i++) {
+        outputStr += getItemHtmlStr(items[i]);
+    }
+    outputEl.innerHTML = outputStr;
+}
 
 // Helper Functions
 sauceBtnEl.addEventListener("click", addSauce);
 function addSauce() {
-    cartArray.push(newItem("Awesome Sauce", 99.99, "Super Awesome Sauce"))
+    items.push(newItem("awesome-sauce.jpg", "Super Awesome Sauce", 99.99))
+    displayItems();
 }
 
-function newItem(itemName, itemPrice, itemDesc) {
+function newItem(itemImg, itemName, itemPrice) {
     return {
+        img: itemImg,
         name: itemName,
         price: itemPrice,
-        desc: itemDesc,
     }
 }
 
-function getItemHtmlStr() {
-
+function getItemHtmlStr(item) {
+    return `
+        <div>
+            <img src=${item.img} width="150px" height="150px">
+            <h2>${items.price}</h2>
+            <p>${item.name}</p>
+        </div>
+    `
 }
